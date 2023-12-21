65bff3c11aa8c605447625e2be20ae9e64a96a59
J. Charles N. M <jcharles.nmbiada@gmail.com>
2023-12-21 18:01:13 +0100

    Trying strategies to connect to the data base server

--- src/configs/db.js
+++ src/configs/db.js
@@ -46,7 +46,7 @@ const operatorsAliases = {
 const db = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password,
   {
   dialect: dbConfig.dialect,
-  host: dbConfig.host,
+  host: 'localhost', // We know that the port is indicated for the local host
   port: dbConfig.port,
   logging: appConfig.env !== 'dev' ? logger.info.bind(logger) : false,
   schema: dbConfig.schema,
@@ -60,9 +60,29 @@ const db = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password,
 })
 
 db.authenticate().then(() => {
-  logger.debug('Connection has been established successfully.');
+  logger.info('Connection has been established successfully to db:' + db.options.host + ' port: ' + db.options.port);
 }).catch((error) => {
-  logger.error('Unable to connect to the database: ', error);
+  logger.error('Unable to connect to the database localhost port ' + db.options.port + ': ', error);
+  logger.info('Does the database is reachable from the configured hostname for port 5432 ?');
+  // Try to change the port
+  db.options.port = 5432
+  // Try to change the host to containered hostname
+  db.options.host = dbConfig.host
+  db.authenticate().then(() => {
+    logger.info('Connection has been established successfully to db:' + db.options.host + ' port: ' + db.options.port);
+  }).catch((error) => {
+    logger.error('Unable to connect to the database ' + db.options.host + ' port ' + db.options.port + ': ', error);
+    logger.info('Does the database is reachable from the defined original hostname and port ?');
+    db.options.host = dbConfig.host
+    db.options.port = dbConfig.port
+    db.authenticate().then(() => {
+      logger.info('Connection has been established successfully to db:' + db.options.host + ' port: ' + db.options.port);
+    }).catch((error) => {
+      logger.error('Unable to connect to the database ' + db.options.host + ' port ' + db.options.port + ': ', error);
+      logger.error('Please configure your links and reboot');
+    });
+  });
+  
 });
 
 export { db };
