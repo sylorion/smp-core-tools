f56569a467a59c349fa1bd98d6798a8424cfcb86
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-05-29 12:11:16 +0200

    ajout de l'esport de SMPevents

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 export { db }           from './src/configs/db.js';
 export { cache } from './src/configs/cache.js';
 export { RabbitMQService } from './src/configs/event.js'
+export {SMPevents} from './src/rabbitMq/index.js'
 export {
     updateContext,
     getUserTokenFromHeaders,
