4fafd6ffdb3864f89680d0cd9f7e65396a7f3894
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-06-02 22:16:43 +0200

    optimisation de la classe rabbit pour gerer les operation crud et correction du bug rabbit sur l'acknowledgement des messages [ATTENTION]: le delais est toujours de 30minutes soit 1 800 000ms

--- src/configs/event.js
+++ src/configs/event.js
@@ -1,49 +1,73 @@
 import amqp from 'amqplib';
 import { createEntityInDatabase, updateEntityInDatabase, deleteEntityFromDatabase } from '../rabbitMq/handlerCRUDOperation.js';
-import { handleEvent } from '../handler/notificationHandler.js';
+import { handleCallback } from '../handler/notificationHandler.js';
 
- class RabbitMQService { // Service RabbitMQ pour la consommation et la publication de messages
-
-  constructor(connectionURL, models, logger = null, durable = true) {
-    console.log(models);  // Cela vous permet de vérifier que les modèles sont passés correctement
-
-    this.connectionURL = connectionURL; // URL de connexion à RabbitMQ
-    this.models = models; // Stocker les modèles passés au service
+/**
+ * RabbitMQService class to manage interactions with RabbitMQ.
+ */
+class RabbitMQService {
+  /**
+   * Constructor to initialize RabbitMQService.
+   * @param {string} connectionURL - URL of the RabbitMQ server.
+   * @param {Object} models - Database models.
+   * @param {Object} mailingService - Mailing service.
+   * @param {Object} brevoMailingConfig - Brevo mailing configuration.
+   * @param {Object} internalNotificationConfig - Internal notification configuration.
+   * @param {Object} [logger=null] - Logger for logging purposes.
+   * @param {boolean} [durable=true] - Whether exchanges and queues are durable.
+   */
+  constructor(connectionURL, models, mailingService, brevoMailingConfig, internalNotificationConfig, logger = null, durable = true) {
+    console.log(models);
+    this.mailingService = mailingService;
+    this.connectionURL = connectionURL;
+    this.models = models;
+    this.brevoMailingConfig = brevoMailingConfig;
+    this.internalNotificationConfig = internalNotificationConfig;
     this.connection = null;
     this.logger = logger;
     this.channel = null;
     this.exchangeDurable = durable;
-    this.queueDurable = durable;
-    this.ack = false;
-  }
-  async connect() { // Connexion à RabbitMQ
-    this.connection = await amqp.connect(this.connectionURL);
-    this.channel = await this.connection.createChannel();
+    this.queueDurable = durable; // Queues are durable by default 
+    this.ack = true; // Acknowledge messages by default to prevent message loss in case of error
   }
 
-  async close() { // Fermeture de la connexion à RabbitMQ
+  /**
+   * Connects to RabbitMQ server.
+   */
+  async connect() {
+    try {
+      this.connection = await amqp.connect(this.connectionURL);
+      this.channel = await this.connection.createChannel();
+      console.log('Connected to RabbitMQ');
+    } catch (error) {
+      console.error('Failed to connect to RabbitMQ:', error);
+    }
+  }
+
+  /**
+   * Closes the RabbitMQ connection.
+   */
+  async close() {
     await this.channel.close();
     await this.connection.close();
   }
 
-
-
   /**
-   * Compares the current microservice's subscriptions against a global configuration (SMPevents) to ensure they are valid.
-   * @param {Object} SMPevents - The global configuration detailing valid event subscriptions for each microservice.
-   * @param {Object} muConsumers - The microservices and their subscribed events to validate.
+   * Verifies the subscriptions against the SMP events.
+   * @param {Object} SMPevents - SMP events configuration.
+   * @param {Object} muConsumers - Microservices consumers configuration.
    */
   async verifySubscriptions(SMPevents, muConsumers) {
     Object.keys(muConsumers).forEach(service => {
       Object.keys(muConsumers[service]).forEach(entity => {
         const operations = muConsumers[service][entity];
         if (!SMPevents[service] || !SMPevents[service][entity]) {
-          console.warn(`Warning: No event configurations found for ${service}.${entity} in SMPevents.`);
+          console.warn(`[Warning]: No event configurations found for ${service}.${entity} in SMPevents.`);
         } else {
           const validOperations = SMPevents[service][entity];
           const invalidOps = operations.filter(op => !validOperations.includes(op));
           if (invalidOps.length > 0) {
-            console.warn(`Warning: Invalid operations ${invalidOps.join(', ')} for ${service}.${entity} not supported by SMPevents.`);
+            console.warn(`[Warning]: Invalid operations ${invalidOps.join(', ')} for ${service}.${entity} not supported by SMPevents.`);
           }
         }
       });
@@ -52,173 +76,164 @@ import { handleEvent } from '../handler/notificationHandler.js';
   }
 
   /**
-   * Souscrit à un échange de topic RabbitMQ pour la consommation des messages.
-   * @param {string} exchangeTopic - Le nom de l'échange de topic.
-   * @param {string} routingKey - La clé de routage pour filtrer les messages. ex : 'user.created'
-   * @param {string} queueName - Le nom de la file de messages à laquelle souscrire.
-   * @param {string} entityName - Le nom de l'entité correspondant aux messages.
-   * @param {string} operation - Le type d'opération CRUD à exécuter (create, update, delete).
-   * @returns {Promise<void>} Une promesse résolue une fois la souscription effectuée.
+   * Subscribes to a RabbitMQ topic.
+   * @param {string} exchangeTopic - Name of the topic exchange.
+   * @param {string} routingKey - Routing key for the exchange.
+   * @param {string} queueName - Name of the queue.
+   * @param {string} entityName - Name of the entity.
+   * @param {string} operation - CRUD operation (created, updated, deleted).
+   * @param {Function} handleCallback - Callback function to handle the message.
+   * @param {Object} data - Data to process.
    */
-  async subscribeTopic(exchangeTopic, routingKey, queueName, entityName, operation) {
-    await this.channel.assertExchange(exchangeTopic, 'topic', { durable: this.exchangeDuration });
-    await this.channel.assertQueue(queueName, { durable: this.queueDuration });
+  async subscribeTopic(exchangeTopic, routingKey, queueName, entityName, operation, handleCallback, data) {
+    await this.channel.assertExchange(exchangeTopic, 'topic', { durable: this.exchangeDurable });
+    await this.channel.assertQueue(queueName, { durable: this.queueDurable });
     await this.channel.bindQueue(queueName, exchangeTopic, routingKey);
-
+  
     this.channel.consume(queueName, (receivedMsg) => {
-      if (receivedMsg) { 
+      if (receivedMsg) {
         const messageData = JSON.parse(receivedMsg.content.toString());
         try {
-           /**
-   * ajout du handlerCrudOperation car le crud des entités qui transitent est commun à toutes les consommations  pour la pertinence des donées dans le systeme
-   * de plus on peut ajouter des fonctionnalités de validation et de transformation des données avant de les
-   * persister dans la base de donnéesen une seule fois pour toutes les entités.
-   * 
-   * de plus ces fonctions de crud sont configurés  depuis le microservice qui les consomme, en effet celui ci choisis l'entité
-   *  et les opérations qu'il veut consommer de celle ci dans son muConsumers.
-  */      handleEvent(routingKey, messageData);
-
-          this.handleCRUDOperation(entityName, operation, messageData); if (this.ack) {
-            this.channel.ack(receivedMsg);
+          console.log(`Processing message with routingKey: ${routingKey} and messageData:`, messageData);
+  
+          const parsedData = JSON.parse(messageData.data);  // Parse the 'data' field
+          const idField = entityName.charAt(0).toLowerCase() + entityName.slice(1) + 'ID';
+          const model = this.models[entityName];
+  
+          // Handle CRUD operations based on 'operation'
+          if (operation === 'created') {
+            createEntityInDatabase(model, parsedData, parsedData[idField]); 
+          } else if (operation === 'updated') {
+            updateEntityInDatabase(model, parsedData, parsedData[idField]);
+          } else if (operation === 'deleted') {
+            deleteEntityFromDatabase(model, parsedData[idField]);
           }
-          /* Utiliser handleEvent pour traiter les messages reçus
-          * cela permet de declanccher la fonction handleEvent pour chaque message reçu
-          * si la rounting key est configuré pour cela
-          */
-          //  handleEvent(routingKey, messageData);
-
-          console.log(`Received message for ${entityName} with operation ${operation}:`, messageData);
+  
+          // Call handleCallback to process notifications
+          handleCallback(entityName, messageData, this.mailingService, this.brevoMailingConfig, this.internalNotificationConfig, this.models.User, this.models.Notification);
+          console.log(`Processed message for ${entityName} with operation ${operation}:`, messageData);
+  
+          // Acknowledge the message
+          this.channel.ack(receivedMsg);
         } catch (error) {
-          console.error(`Error handling CRUD operation for ${entityName}: ${error}`);
-          this.channel.nack(receivedMsg);
+          console.error(`Error processing message for ${entityName}: ${error}`);
+          // Nack the message and do not requeue
+          this.channel.nack(receivedMsg, false, false); // nack with requeue set to false
+  
+          // Log the error or send a notification
+          if (this.logger) {
+            this.logger.error(`Error processing message for ${entityName}: ${error}`);
+          } else {
+            console.error(`Error processing message for ${entityName}: ${error}`);
+          }
         }
       }
-    });
+    }, { noAck: false }); // Acknowledge messages to prevent message loss
   }
+  
 
-  async subscribeDirect(exchange, routingKey, queueName, callback) { 
-    // xs 
-    await this.channel.assertExchange(exchange, 'direct', { durable: this.exchangeDuration });
-    await this.channel.assertQueue(queueName, { durable: this.queueDuration });
+  /**
+   * Subscribes to a RabbitMQ direct exchange.
+   * @param {string} exchange - Name of the direct exchange.
+   * @param {string} routingKey - Routing key for the exchange.
+   * @param {string} queueName - Name of the queue.
+   * @param {Function} callback - Callback function to handle the message.
+   */
+  async subscribeDirect(exchange, routingKey, queueName, callback) {
+    await this.channel.assertExchange(exchange, 'direct', { durable: this.exchangeDurable });
+    await this.channel.assertQueue(queueName, { durable: this.queueDurable });
     await this.channel.bindQueue(queueName, exchange, routingKey);
 
     this.channel.consume(queueName, (receivedMsg) => {
       if (receivedMsg) {
-        // Maybe return receivedMsg directly
         try {
            callback(receivedMsg.content);
-          if (this.ack) {
-            this.channel.ack(receivedMsg);
-          }
-        } catch (error) {
-          console.error('Unable to read the  message : ', error);
-          // push back to the  Queue ?
-          if (this.ack) {
-            this.channel.nack(receivedMsg);
-          } else {
-            if (this.logger) {
-              logger.error('Msg lost ', receivedMsg);
-            } else {
-              console.error('Msg lost ', receivedMsg);
-            }
-          }
+        // Acknowledge the message
+        this.channel.ack(receivedMsg);
+      } catch (error) {
+        console.error('Unable to read the message:', error);
+        // Nack the message and do not requeue
+        this.channel.nack(receivedMsg, false, false); // nack with requeue set to false
+
+        // Log the error or send a notification
+        if (this.logger) {
+          this.logger.error('Msg lost:', receivedMsg);
+        } else {
+          console.error('Msg lost:', receivedMsg);
         }
       }
-    });
-  }
+    }
+  }, { noAck: false });
+}
 
   /**
-   * Publishes a message to a topic exchange with the specified routing key.
-   *
-   * @param {string} exchangeTopic - The name of the topic exchange.
-   * @param {string} routingKey - The routing key for the message.
-   * @param {any} message - The message to be published.
-   * @param {object} [context={}] - Additional context for the message.
-   * @param {object} [options={}] - Additional options for publishing the message.
-   * @returns {Promise<void>} - A promise that resolves when the message is published.
+   * Publishes a message to a RabbitMQ topic.
+   * @param {string} exchangeTopic - Name of the topic exchange.
+   * @param {string} routingKey - Routing key for the exchange.
+   * @param {Object} message - Message to publish.
+   * @param {Object} [context={}] - Context of the message.
+   * @param {Object} [options={}] - Additional options for publishing.
    */
   async publishTopic(exchangeTopic, routingKey, message, context = {}, options = {}) {
-    // Build the message with request ID, user ID and so on before transmitting
     const messageWithContext = {
       context: context,
       data: message
     };
     const messageBuffer = Buffer.from(JSON.stringify(messageWithContext));
-    
+
     await this.channel.assertExchange(exchangeTopic, 'topic', { durable: options.durable ?? this.exchangeDurable });
     this.channel.publish(exchangeTopic, routingKey, messageBuffer, options);
   }
 
+  /**
+   * Prepares to publish a message to a RabbitMQ topic by connecting, publishing, and closing the connection.
+   * @param {string} exchangeTopic - Name of the topic exchange.
+   * @param {string} routingKey - Routing key for the exchange.
+   * @param {Object} message - Message to publish.
+   * @param {Object} [context={}] - Context of the message.
+   * @param {Object} [options={}] - Additional options for publishing.
+   */
   async readyToPublishTopic(exchangeTopic, routingKey, message, context = {}, options = {}) {
-    this.connect();
-    this.publishTopic(exchangeTopic, routingKey, message, context, options);
-    this.close
+    await this.connect();
+    await this.publishTopic(exchangeTopic, routingKey, message, context, options);
+    await this.close();
   }
 
   /**
- * Démarre les consommateurs RabbitMQ en fonction de la configuration des microservices.
- * @param {Object} microservices - La configuration des échanges et des entités à consommer pour chaque microservice.
- * @returns {Promise<void>} Une promesse résolue une fois les consommateurs configurés.
- */
+   * Starts consumers for the specified microservices.
+   * @param {Object} microservices - Configuration of microservices and their operations.
+   */
   async startConsumers(microservices) {
-    const µservice = process.env.SMP_MU_SERVICE_NAME; // Nom du microservice stocké dans la variable d'environnement
+    const µservice = process.env.SMP_MU_SERVICE_NAME;
 
     await this.connect();
     for (const [microserviceName, config] of Object.entries(microservices)) {
-      for (const [entityName, actions] of Object.entries(config)) {
-        for (const action of actions) {
-          const queueName = `${entityName}-${action}-${µservice}-queue`; // Utiliser le nom du microservice, le nom de l'entité et l'action dans le nom de la file d'attente
-          const routingKey = `${entityName}.${action}`;
-          const exchangeName = `${microserviceName}.events`; // Utiliser le nom du microservice pour le nom de l'échange
+      for (const [entityName, operations] of Object.entries(config)) {
+        for (const operation of operations) {
+          const queueName = `${entityName}-${operation}-${µservice}-queue`;
+          const routingKey = `${entityName}.${operation}`;
+          const exchangeName = `${microserviceName}.events`;
+
           console.log(`Preparing to subscribe to queue ${queueName} ...`);
-          this.subscribeTopic(exchangeName, routingKey, queueName, entityName, action);
+
+          this.subscribeTopic(exchangeName, routingKey, queueName, entityName, operation, handleCallback);
+          /*  here the subscribeTopic function is called with the following parameters:
+              - exchangeName: the name of the exchange to bind to
+              - routingKey: the routing key to use for binding
+              - queueName: the name of the queue to bind to
+              - entityName: the name of the entity to process
+              - operation: the operation to process (created, updated, deleted)
+              - handleCallback: the callback function to handle the message
+              - data: the data to process
+
+              this serves to subscribe to the specified exchange, bind the queue to it, and consume messages from the queue    
+          */
+
         }
       }
     }
     console.log("All consumers have been set up for all microservices.");
   }
- /**
- * Handle CRUD operations for a given entity based on operation type.
- * @param {string} entityName - The name of the entity for CRUD operation.
- * @param {string} operation - Type of CRUD operation ('created', 'updated', 'deleted').
- * @param {Object} messageData - Data associated with the CRUD operation.
- * @returns {Promise<Object>} A promise resolved with the result of the CRUD operation.
- * @throws {Error} If the model is not found or if the operation is not supported.
- */
-async handleCRUDOperation(entityName, operation, messageData) {
-  const model = this.models[entityName];
-
-  // Parse the incoming JSON data
-  let data = JSON.parse(messageData.data);
-
-  // Dynamically construct the ID field name based on the entity name
-  const idField = entityName.charAt(0).toLowerCase() + entityName.slice(1) + 'ID';
-
-  // Correctly log the value of the dynamically named ID field
-  console.log( idField, data[idField]);
-
-  if (!model) {
-    throw new Error(`Model not found for entity: ${entityName}`);
-  }
-
-  switch (operation) { // Execute the appropriate CRUD operation based on the operation type
-    case 'created':
-      return createEntityInDatabase(model, data, data[idField]);
-    case 'updated':
-      // Ensure the ID for update operation is fetched dynamically from the data object
-      return updateEntityInDatabase(model, data, data[idField]);
-    case 'deleted':
-      // Ensure the ID for delete operation is fetched dynamically from the data object
-      return deleteEntityFromDatabase(model, data[idField]);
-    default:
-      console.error(`Operation '${operation}' not supported for entity '${entityName}'.`);
-      throw new Error(`Unsupported operation: ${operation}`);
-  }
 }
 
-
-}
-
-export  { RabbitMQService };
-
-
+export { RabbitMQService };
