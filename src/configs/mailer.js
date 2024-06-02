432a550e93e4e1afb816acbbac22da7b92e2a969
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-06-03 00:57:11 +0200

    modification de l'architecture des classes de mailing, divison dans d'autres fichier et meilleur referencement

--- src/configs/mailer.js
+++ src/configs/mailer.js
@@ -1,6 +1,5 @@
 import nodemailer from "nodemailer";
 import retry from "async-retry";
-import  BrevoMailingService  from "../brevo/brevo.js"; // Assurez-vous que le chemin est correct.
 
 /**
  * Abstract class representing a mailing service.
@@ -16,41 +15,6 @@ class MailingService {
    */
   sendMail(to, subject, html, options) {
     throw new Error("Cette méthode doit être implémentée par la sous-classe");
-  }
-}
-
-
-/**
- * Factory class for creating mailing service instances based on environment configuration.
- * Creates and returns an instance of a mailing service based on the MAIL_SERVICE environment variable.
- * @returns {MailingService} An instance of the appropriate subclass of MailingService.
- */
-
-class MailingServiceFactory {
-  static createMailingService(apiKey, brevoMailingConfig, internalNotificationConfig) {
-    console.log("OKE");
-
-    console.log("OKE", process.env.MAIL_SERVICE);
-
-    switch (process.env.MAIL_SERVICE) {
-      case "Brevo":
-        return new BrevoMailingService(apiKey, brevoMailingConfig);
-        default:
-        const smtpConfig = {
-          host: process.env.SMTP_HOST,
-          port: parseInt(process.env.SMTP_PORT, 10),
-          secure: process.env.SMTP_SECURE === "true", // Convertit la chaîne en boolean
-          auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-          },
-        };
-        console.log("OKE", process.env.MAIL_SERVICE);
-
-        return new SMTPMailingService(smtpConfig);
-        
-    }
-
   }
 }
 
@@ -70,10 +34,6 @@ class SMTPMailingService extends MailingService {
 
   /**
    * Sends an email using SMTP with retry logic.
-   * Logique d'envoi d'email via SMTP
-   * Utilisez le module nodemailer pour envoyer l'email
-   * Utilisez la configuration SMTP fournie pour configurer le transporteur
-   * Utilisez la méthode sendMail du transporteur pour envoyer l'email
    * @param {string|string[]} to - The recipient(s) of the email.
    * @param {string} subject - The subject of the email.
    * @param {string} html - The HTML content of the email.
@@ -95,18 +55,15 @@ class SMTPMailingService extends MailingService {
       async (bail) => {
         try {
           const transporter = nodemailer.createTransport(this.smtpConfig);
-          // Create a new transporter for each attempt
           const info = await transporter.sendMail(mailOptions);
 
           console.log("Mail sent: %s", info.messageId);
         } catch (error) {
           if (error.responseCode === 550) {
-            bail(
-              new Error("Critical failure, stopping retry: " + error.message)
-            ); // Stops retrying after a critical failure
+            bail(new Error("Critical failure, stopping retry: " + error.message));
           } else {
             console.error("Failed to send email via SMTP:", error);
-            throw error; // This will trigger a retry unless max retries have been reached
+            throw error;
           }
         }
       },
@@ -114,11 +71,10 @@ class SMTPMailingService extends MailingService {
         retries: 3,
         factor: 2,
         minTimeout: 1000,
-        onRetry: (error, attempt) =>
-          console.log(`Tentative ${attempt}: ${error}`),
+        onRetry: (error, attempt) => console.log(`Tentative ${attempt}: ${error}`),
       }
     );
   }
 }
 
-export { MailingServiceFactory, MailingService };
+export { MailingService, SMTPMailingService };
