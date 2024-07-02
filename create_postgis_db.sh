d3d79d1aa89f927506747c92e7ba398ee6e8d956
J. Charles N. M <jcharles.nmbiada@gmail.com>
2024-07-02 02:13:54 +0200

    Documentation update and rebase from dev

--- create_postgis_db.sh
+++ create_postgis_db.sh
@@ -11,7 +11,7 @@ DATABASES=("smp_gateway" "smp_location" "smp_organization" "smp_document" "smp_u
 USERS=("smp_gateway_u" "smp_location_u" "smp_organization_u" "smp_document_u" "smp_userspace_u" "smp_catalog_u" "smp_accounting_u" "smp_audits_u" "smp_reviews_u" "smp_notification_u" "smp_authentication_u" "smp_comment_u" "smp_recsys_u" "smp_webapp_u" )  # Liste prédéfinie de noms de bases de données
 # Liste prédéfinie de mots de passe
 PASSWORDS=("smp_gateway_u_pswd" "smp_location_u_pswd" "smp_organization_u_pswd" "smp_document_u_pswd" "smp_userspace_u_pswd" "smp_catalog_u_pswd" "smp_accounting_u_pswd" "smp_audits_u_pswd" "smp_reviews_u_pswd" "smp_notification_u_pswd" "smp_authentication_u_pswd" "smp_comment_u_pswd" "smp_recsys_u_pswd" "smp_webapp_u_pswd" )  # Liste prédéfinie de noms de bases de données
-
+# Liste prédéfinie d'extension à installer ; pg_partman ne semble pas être présent par défaut
 EXTENSIONS=("postgis" "pg_stat_statements" "pg_partman" "pg_prewarm")  # Extensions à installer
 
 # Vérification que les listes ont la même longueur
@@ -20,6 +20,7 @@ if [[ ${#DATABASES[@]} != ${#USERS[@]} || ${#DATABASES[@]} != ${#PASSWORDS[@]} ]
     exit 1
 fi
 
+# Pour éviter d'entrer le mot de passe postgres à chaque commande pgsql ... 
 echo "$HOST:$PORT:*:$USER:${PASSWORD}" > ~/.pgpass
 chmod 600 ~/.pgpass
 
