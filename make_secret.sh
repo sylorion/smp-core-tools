96d237e65b4a00e70a2865ebe70c1ec8d8287f9d
J. Charles N. M <jcharles.nmbiada@gmail.com>
2024-08-23 09:39:28 +0200

    Update make_secret.sh script

--- make_secret.sh
+++ make_secret.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -e
-
+# Usage make_secrets <SERVICE NAME> <DATA BASE HOSTNAME> <SECRETS FOLDER> 
 #
-SMP_ROOT_SECRETS_FOLDER="./run/secrets"
+SERVICE_NAME=${1:-"gateway"}
+DBMS_NODE_HOSTNAME=${2:-"node182572-dev-dbms.sh1.hidora.net"}
+
+SMP_ROOT_SECRETS_FOLDER=${3:-"~/run/secrets"}
 # Create the directory if it does not exist
 mkdir -p $SMP_ROOT_SECRETS_FOLDER
 # Write predefined values to each file
-echo "docker181245-dev-postgis.sh1.hidora.net" > "${SMP_ROOT_SECRETS_FOLDER}/db_host" 
-echo "5432" > "${SMP_ROOT_SECRETS_FOLDER}/db_port" 
-echo "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_freezed" 
-echo "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_timestamp" 
-echo "public" > "${SMP_ROOT_SECRETS_FOLDER}/db_schema" 
-echo "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_sync" 
-echo "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_paranoid" 
-echo "postgres" > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_name" 
-echo "postgres" > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_username" 
-echo "b@$icP@$$word" > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_pswd" 
-echo "postgres" > "${SMP_ROOT_SECRETS_FOLDER}/db_dialect" 
-echo "console.log" > "${SMP_ROOT_SECRETS_FOLDER}/db_logging" 
+echo -n "${DBMS_NODE_HOSTNAME}" > "${SMP_ROOT_SECRETS_FOLDER}/db_host"
+echo -n "5432" > "${SMP_ROOT_SECRETS_FOLDER}/db_port"
+echo -n "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_sync"
+echo -n "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_paranoid"
+echo -n "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_timestamp"
+echo -n "true" > "${SMP_ROOT_SECRETS_FOLDER}/db_freezed_table_name"
+echo -n "public" > "${SMP_ROOT_SECRETS_FOLDER}/db_schema"
+echo -n "smp_${SERVICE_NAME}" > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_name"
+echo -n "smp_${SERVICE_NAME}_u" - > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_username"
+echo -n "smp_${SERVICE_NAME}_u_pswd" > "${SMP_ROOT_SECRETS_FOLDER}/mu_db_pswd"
+echo "postgres" > "${SMP_ROOT_SECRETS_FOLDER}/db_dialect"
+echo "console.log" > "${SMP_ROOT_SECRETS_FOLDER}/db_logging"
 echo "" > "${SMP_ROOT_SECRETS_FOLDER}/db_pool"
 # Print a message indicating the operation is complete
 echo "Secrets have been written to $SMP_ROOT_SECRETS_FOLDER"
