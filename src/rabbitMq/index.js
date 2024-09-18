e3d38e15218d74fdd233ad2b9bc32f6ce6224905
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-09-18 17:11:23 +0200

    creation d'un callbacl manager qui va se charger de buil der les callback de chaque event via configureEntityCallbacks, ce callback manager va se charger d'appeler les focntions de crud en fonction de l'operation et de l'entité dont il est question. dans ce fichier il ya une fonciton ;getCrudCallbacks qui permet d'appeler le callaback de crud en renseignant le nom de l'entité en parametre.

--- src/rabbitMq/index.js
+++ src/rabbitMq/index.js
@@ -6,7 +6,7 @@ const ACTIONS = {
   CREATED: 'created',
   UPDATED: 'updated',
   DELETED: 'deleted',
-  VISITED: 'visited',  
+  VISITED: 'visited',
   LISTED: 'listed',
 };
 
@@ -14,160 +14,90 @@ Object.freeze(ACTIONS);
 
 /**
  * Génère les opérations CRUD pour une entité donnée et permet l'ajout d'événements personnalisés.
- * @param {string} name - Le nom de l'entité (e.g., 'User', 'Profile').
+ * @param {string} entityName - Le nom de l'entité (e.g., 'User', 'Profile').
  * @param {Object} [customEvents={}] - Un objet représentant les événements personnalisés.
  * @returns {Object} - Objet contenant les actions CRUD et les événements personnalisés.
  */
-function generateCrudOperations(name, customEvents = {}) {
+function generateCrudOperations(entityName, customEvents = {}) {
   const defaultEvents = {
-    created: `${name}.${ACTIONS.CREATED}`,
-    updated: `${name}.${ACTIONS.UPDATED}`,
-    deleted: `${name}.${ACTIONS.DELETED}`,
-    visited: `${name}.${ACTIONS.VISITED}`,
-    listed: `${name}.${ACTIONS.LISTED}`,
+    created: `${entityName}.${ACTIONS.CREATED}`,
+    updated: `${entityName}.${ACTIONS.UPDATED}`,
+    deleted: `${entityName}.${ACTIONS.DELETED}`,
+    visited: `${entityName}.${ACTIONS.VISITED}`,
+    listed: `${entityName}.${ACTIONS.LISTED}`,
   };
 
-  // Combiner les événements standards avec les événements personnalisés
   return {
     ...defaultEvents,
-    ...customEvents
+    ...customEvents,
   };
 }
 
 /**
  * Configuration des événements pour chaque entité des différents microservices,
- * accessible directement via `SMPEvents.User.visited` ou `SMPEvents.UserToken.resetToken`.
+ * accessible via des chemins comme `SMPEvents.UserSpace.User.created`.
  */
 const SMPEvents = {
-
-  // [MU-USERSPACE]
-  User: {
-    ...generateCrudOperations('User', { deactivated: 'User.deactivated' })  
-  },
-  Profile: {
-    ...generateCrudOperations('Profile')
-  },
-  PaymentMethod: {
-    ...generateCrudOperations('PaymentMethod')
-  },
-  PaymentConfig: {
-    ...generateCrudOperations('PaymentConfig')
-  },
-  Role: {
-    ...generateCrudOperations('Role')
-  },
-  UserPreference: {
-    ...generateCrudOperations('UserPreference')
+  UserSpace: {
+    User: generateCrudOperations('User', { deactivated: 'User.deactivated' }),
+    Profile: generateCrudOperations('Profile'),
+    PaymentMethod: generateCrudOperations('PaymentMethod'),
+    PaymentConfig: generateCrudOperations('PaymentConfig'),
+    Role: generateCrudOperations('Role'),
+    UserPreference: generateCrudOperations('UserPreference'),
   },
 
-  // [MU-ORGANIZATION]
   Organization: {
-    ...generateCrudOperations('Organization')
-  },
-  UserOrganization: {
-    ...generateCrudOperations('UserOrganization')
-  },
-  TermsAndConditions: {
-    ...generateCrudOperations('TermsAndConditions')
-  },
-  FaqOrganization: {
-    ...generateCrudOperations('FaqOrganization')
-  },
-  OrganizationMedia: {
-    ...generateCrudOperations('OrganizationMedia')
-  },
-  Industry: {
-    ...generateCrudOperations('Industry')
-  },
-  TagOrganization: {
-    ...generateCrudOperations('TagOrganization')
-  },
-  TopicOrganization: {
-    ...generateCrudOperations('TopicOrganization')
+    Organization: generateCrudOperations('Organization'),
+    UserOrganization: generateCrudOperations('UserOrganization'),
+    TermsAndConditions: generateCrudOperations('TermsAndConditions'),
+    FaqOrganization: generateCrudOperations('FaqOrganization'),
+    OrganizationMedia: generateCrudOperations('OrganizationMedia'),
+    Industry: generateCrudOperations('Industry'),
+    TagOrganization: generateCrudOperations('TagOrganization'),
+    TopicOrganization: generateCrudOperations('TopicOrganization'),
   },
 
-  // [MU-CATALOG]
-  Service: {
-    ...generateCrudOperations('Service')
-  },
-  Criteria: {
-    ...generateCrudOperations('Criteria')
-  },
-  Asset: {
-    ...generateCrudOperations('Asset')
-  },
-  ServiceAsset: {
-    ...generateCrudOperations('ServiceAsset')
-  },
-  ServiceMedia: {
-    ...generateCrudOperations('ServiceMedia')
-  },
-  FaqAnswer: {
-    ...generateCrudOperations('FaqAnswer')
-  },
-  FaqQuestion: {
-    ...generateCrudOperations('FaqQuestion')
-  },
-  FaqService: {
-    ...generateCrudOperations('FaqService')
-  },
-  ServiceAttribute: {
-    ...generateCrudOperations('ServiceAttribute')
-  },
-  Topic: {
-    ...generateCrudOperations('Topic')
-  },
-  Tag: {
-    ...generateCrudOperations('Tag')
+  Catalog: {
+    Service: generateCrudOperations('Service'),
+    Criteria: generateCrudOperations('Criteria'),
+    Asset: generateCrudOperations('Asset'),
+    ServiceAsset: generateCrudOperations('ServiceAsset'),
+    ServiceMedia: generateCrudOperations('ServiceMedia'),
+    FaqAnswer: generateCrudOperations('FaqAnswer'),
+    FaqQuestion: generateCrudOperations('FaqQuestion'),
+    FaqService: generateCrudOperations('FaqService'),
+    ServiceAttribute: generateCrudOperations('ServiceAttribute'),
+    Topic: generateCrudOperations('Topic'),
+    Tag: generateCrudOperations('Tag'),
   },
 
-  // [MU-ACCOUNTING]
-  Invoice: {
-    ...generateCrudOperations('Invoice')
-  },
-  Estimate: {
-    ...generateCrudOperations('Estimate')
-  },
-  Transaction: {
-    ...generateCrudOperations('Transaction')
-  },
-  EstimateAsset: {
-    ...generateCrudOperations('EstimateAsset')
+  Accounting: {
+    Invoice: generateCrudOperations('Invoice'),
+    Estimate: generateCrudOperations('Estimate'),
+    Transaction: generateCrudOperations('Transaction'),
+    EstimateAsset: generateCrudOperations('EstimateAsset'),
   },
 
-  // [MU-NOTIFICATION]
   Notification: {
-    ...generateCrudOperations('Notification')
-  },
-  NotificationTemplate: {
-    ...generateCrudOperations('NotificationTemplate')
+    Notification: generateCrudOperations('Notification'),
+    NotificationTemplate: generateCrudOperations('NotificationTemplate'),
   },
 
-  // [MU-REVIEWCOMMENT]
-  Comment: {
-    ...generateCrudOperations('Comment')
-  },
-  Review: {
-    ...generateCrudOperations('Review')
+  ReviewComment: {
+    Comment: generateCrudOperations('Comment'),
+    Review: generateCrudOperations('Review'),
   },
 
-  // [MU-AUTHENTICATION]
-  Application: {
-    ...generateCrudOperations('Application')
-  },
-  ApplicationToken: {
-    ...generateCrudOperations('ApplicationToken') 
-  },
-  UserToken: {
-    ...generateCrudOperations('UserToken')  
-  },
-  ResetPasswordToken: {
-    ...generateCrudOperations('ResetPasswordToken')  
+  Authentication: {
+    Application: generateCrudOperations('Application'),
+    ApplicationToken: generateCrudOperations('ApplicationToken'),
+    UserToken: generateCrudOperations('UserToken'),
+    ResetPasswordToken: generateCrudOperations('ResetPasswordToken'),
   },
 
-  // [MU-DOCUMENT]
-  Media: {
-    ...generateCrudOperations('Media')
+  Document: {
+    Media: generateCrudOperations('Media'),
   },
 };
 
