a4fbc8867405bba763f7ba9de22e4d2feaf30d8f
J. Charles N. M <jcnm@sylorion.com>
2024-09-22 13:38:51 +0200

    Bugfix pute key in front of appTokens nb 4

--- src/configs/appTokens.js
+++ src/configs/appTokens.js
@@ -1,23 +1,23 @@
 const appTokens = {
     f2655ffda8594852: {
         name: "SMP WebApp",
-        key: 'TA7Vin/JY0YIp9sGpiy6d7ade351Ub+Ia3Pj1acdMb7AxKL/t1vVCcXt6NSaEiTfYbCes1b4Qs8l54buR17oQdsP9p0lpx0ojKaSdjzER9ftagPpr/5byPZhyxsQNU/V9dzoIx4eVV2sSiuFq4XFNL48v6wZz3znX4IlLenGji8=',
+        key: "TA7Vin/JY0YIp9sGpiy6d7ade351Ub+Ia3Pj1acdMb7AxKL/t1vVCcXt6NSaEiTfYbCes1b4Qs8l54buR17oQdsP9p0lpx0ojKaSdjzER9ftagPpr/5byPZhyxsQNU/V9dzoIx4eVV2sSiuFq4XFNL48v6wZz3znX4IlLenGji8=",
     },
     ed5d8f325bb891b0: {
         name: "SMP iOS App",
-        key: 'DvmgzMqfGDlK+T4HRE/7CNJ9cj0kV/Q9gTP+qnb4SVlY6lqzjO+HdtlqJqAK72OcIgEYBfvoHGTTInSjbi8mdwsY9K2CsA/DQ0yfhBtzeNULOFeHjLjC51Sh0IjNeCksrm1f//geVhgqIMuoBUoWkDV+2OtfRCFuATMyyIS+15E=',
+        key: "DvmgzMqfGDlK+T4HRE/7CNJ9cj0kV/Q9gTP+qnb4SVlY6lqzjO+HdtlqJqAK72OcIgEYBfvoHGTTInSjbi8mdwsY9K2CsA/DQ0yfhBtzeNULOFeHjLjC51Sh0IjNeCksrm1f//geVhgqIMuoBUoWkDV+2OtfRCFuATMyyIS+15E=",
     },
     b779f872923d0200: {
         name: "SMP Android App",
-        key: 'upUV4TTTUZY3MM5Dw7FO0f4UPlCUby0zuB9MgHBqWdxHAKMNni4Rf3dLumstE9BqrlCiQJABmgO5kpbMrYHs04NJqB8sB8u7cRtzTSdNkAT9O1CNcnfRU3uY755tVbban47BE2epopqOhwY9Gx1cGG04lRJT+vSEumOGJT4LMlQ=',
+        key: "upUV4TTTUZY3MM5Dw7FO0f4UPlCUby0zuB9MgHBqWdxHAKMNni4Rf3dLumstE9BqrlCiQJABmgO5kpbMrYHs04NJqB8sB8u7cRtzTSdNkAT9O1CNcnfRU3uY755tVbban47BE2epopqOhwY9Gx1cGG04lRJT+vSEumOGJT4LMlQ=",
     },
     ca69df3302d626b5: {
         name: "SMP Android App",
-        'McHoj6Zm3zENoRwrMimygtKuNoy749hU7zP0wMFsAxrklZVXP5Fln4O4h1vk39gFFji5V444HRXO53VvAMBztdnW4Yhfi1A0dn7ftNKFwPC6HZ7iv3RQQn3btzNKdSpGXIVL1elRQnuykXE/q17GDE76rGpjdJvR9HP6W/rEi30=',
+        key: "McHoj6Zm3zENoRwrMimygtKuNoy749hU7zP0wMFsAxrklZVXP5Fln4O4h1vk39gFFji5V444HRXO53VvAMBztdnW4Yhfi1A0dn7ftNKFwPC6HZ7iv3RQQn3btzNKdSpGXIVL1elRQnuykXE/q17GDE76rGpjdJvR9HP6W/rEi30=",
     },
     f1060459e5d4c78b: {
         name: "Our first developer client App",
-        key: 'VQAJb/N67VuhIGDgCzFJmSAOUxEL7jJxxDNO7SHujl09U9QPSt0ZtolHulcUX2Xl+CxZGTMGyerI2uhKsnc9GCnt4teKa+F6STolYnDlW3YALkcz9bvoG5g6vo6Zgkp9O+7jcfxPUsZV2NlFTDmKSvWP7CprBFxYYrH++8jjMN0=',
+        key: "VQAJb/N67VuhIGDgCzFJmSAOUxEL7jJxxDNO7SHujl09U9QPSt0ZtolHulcUX2Xl+CxZGTMGyerI2uhKsnc9GCnt4teKa+F6STolYnDlW3YALkcz9bvoG5g6vo6Zgkp9O+7jcfxPUsZV2NlFTDmKSvWP7CprBFxYYrH++8jjMN0=",
     }
 }
 
