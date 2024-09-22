f99ac90218cae752e65b231e70a9c94147d0f730
J. Charles N. M <jcharles.nmbiada@gmail.com>
2024-09-22 11:37:28 +0200

    Save the data to data base

--- src/utils/entityMutation.js
+++ src/utils/entityMutation.js
@@ -72,7 +72,7 @@ async function entityCreator(entityContext, inputs, appContext) {
 
     const dbOptions = appendLoggingContext({}, appContext);
     let entity = mEntity;
-    // entity = await entityContext.inputsCommitCallBackFn(mEntity, dbOptions);
+    entity = await entityContext.inputsCommitCallBackFn(mEntity, dbOptions);
     if (!entity) {
       throw new SMPError(`Failed to create ${entityContext.entityName}`, entityContext.errorCodeEntityCreationFaillure || 'ERROR_ENTITY_CREATION_FAILED');
     }
