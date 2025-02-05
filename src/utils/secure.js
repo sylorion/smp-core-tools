52a0667e24a79883fb338fde7ed4a96c017158dc
J. Charles N. M <jcnm@sylorion.com>
2025-02-05 17:13:03 +0100

    Update encryption function to use default iv and encryption key

--- src/utils/secure.js
+++ src/utils/secure.js
@@ -2,10 +2,10 @@ import { randomBytes, createCipheriv, createDecipheriv} from 'crypto';
 
 const algorithm = 'aes-256-cbc';
 const key = process.env.ENCRYPTION_KEY ?? "12345678901234567890123456789012";
-const iv = randomBytes(16);
+const iv = process.env.ENCRYPTION_IV_KEY ?? randomBytes(16);
 
 // Fonction de chiffrement
-export function encryptData(data, iv, key) {
+export function encryptData(data, iv = iv, key = key) {
   const cipher = createCipheriv(algorithm, Buffer.from(key), iv);
   let encrypted = cipher.update
   (data, 'utf8', 'hex');
@@ -14,7 +14,7 @@ export function encryptData(data, iv, key) {
 }
 
 // Fonction de déchiffrement
-export function decryptData(encryptedData, iv, key) {
+export function decryptData(encryptedData, iv = iv, key = key) {
   const decipher = createDecipheriv(algorithm, Buffer.from(key), iv);
   let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
