850e6ac9d267630e933cabd6579b1253a28a3f2d
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-03-19 14:59:42 +0100

    optimisation du producer avec une nouvelle approche stars qui permet de linker une queue a un enssembnle de rk, le formattage des Rk se fait aussi dans l'evendproducer desormais

--- src/rabbitMq/eventProducers.js
+++ src/rabbitMq/eventProducers.js
@@ -12,35 +12,40 @@ const ACTIONS = {
 
 Object.freeze(ACTIONS);
 
-/**
- * Génère les opérations CRUD pour une entité donnée et permet l'ajout d'événements personnalisés.
- * @param {string} entityName - Le nom de l'entité (e.g., 'User', 'Profile').
- * @param {Object} [customEvents={}] - Un objet représentant les événements personnalisés.
- * @returns {Object} - Objet contenant les actions CRUD et les événements personnalisés.
- */
 function generateCrudOperations(entityName, customEvents = {}) {
+  const normalized = entityName.toLowerCase();
   const defaultEvents = {
-    created: `${entityName}.${ACTIONS.CREATED}`,
-    updated: `${entityName}.${ACTIONS.UPDATED}`,
-    deleted: `${entityName}.${ACTIONS.DELETED}`,
-    visited: `${entityName}.${ACTIONS.VISITED}`,
-    listed: `${entityName}.${ACTIONS.LISTED}`,
+    created: `rk.${normalized}.${ACTIONS.CREATED}`,
+    updated: `rk.${normalized}.${ACTIONS.UPDATED}`,
+    deleted: `rk.${normalized}.${ACTIONS.DELETED}`,
+    visited: `rk.${normalized}.${ACTIONS.VISITED}`,
+    listed: `rk.${normalized}.${ACTIONS.LISTED}`,
   };
 
-  return {
-    ...defaultEvents,
-    ...customEvents,
-  };
+  const events = { ...defaultEvents, ...customEvents };
+
+  if ('stars' in customEvents) {
+    let starVal = customEvents.stars;
+    starVal = starVal.toLowerCase();
+    if (!starVal.startsWith('rk.')) {
+      starVal = `rk.${starVal}`;
+    }
+    events.stars = starVal;
+  } else {
+    events.stars = `rk.${normalized}.*`;
+  }
+
+  return events;
 }
 
 /**
- * Configuration des événements pour chaque entité des différents microservices,
- * accessible via des chemins comme `SMPEvents.UserSpace.User.created`.
+ * Configuration des événements pour chaque entité des différents microservices.
  */
 const SMPEvents = {
   UserSpace: {
-    User: generateCrudOperations('UserSpace.User', { deactivated: 'User.deactivated' }),
-    UserRole: generateCrudOperations('UserSpace.UserRole', { dismissed: 'UserRole.dismissed' }),
+    stars: 'userspace.*',
+    User: generateCrudOperations('UserSpace.User', { deactivated: 'rk.userspace.user.deactivated' }),
+    UserRole: generateCrudOperations('UserSpace.UserRole', { dismissed: 'rk.userspace.userrole.dismissed' }),
     Profile: generateCrudOperations('UserSpace.Profile'),
     PaymentMethod: generateCrudOperations('UserSpace.PaymentMethod'),
     PaymentConfig: generateCrudOperations('UserSpace.PaymentConfig'),
@@ -49,6 +54,7 @@ const SMPEvents = {
   },
 
   Organization: {
+    stars: 'organization.*',
     Organization: generateCrudOperations('Organization.Organization'),
     UserOrganization: generateCrudOperations('Organization.UserOrganization'),
     TermsAndConditions: generateCrudOperations('Organization.TermsAndConditions'),
@@ -57,13 +63,21 @@ const SMPEvents = {
     Industry: generateCrudOperations('Organization.Industry'),
     TagOrganization: generateCrudOperations('Organization.TagOrganization'),
     TopicOrganization: generateCrudOperations('Organization.TopicOrganization'),
-    JointOrganizationInvitation: { sent: 'Organization.JointOrganizationInvitation.sent' },
-    MemberOrganization: { invited: 'Organization.MemberOrganization.invited', removed: 'Organization.MemberOrganization.removed', updated: 'Organization.MemberOrganization.updated' },
-
+    JointOrganizationInvitation: { sent: 'rk.organization.jointorganizationinvitation.sent' },
+    MemberOrganization: {
+      stars: "rk.organization.memberorganization.*", // Définit explicitement le wildcard pour MemberOrganization
+      invited: 'rk.organization.memberorganization.invited',
+      removed: 'rk.organization.memberorganization.removed',
+      updated: 'rk.organization.memberorganization.updated'
+    },
   },
 
   Catalog: {
-    Service: generateCrudOperations('Catalog.Service' , {addedToFavorite: 'Catalog.Service.addedToFavorite'}, {removedFromFavorite: 'Catalog.Service.removedFromFavorite'}),
+    stars: 'catalog.*',
+    Service: generateCrudOperations('Catalog.Service', { 
+      addedToFavorite: 'rk.catalog.service.addedtofavorite',
+      removedFromFavorite: 'rk.catalog.service.removedfromfavorite'
+    }),
     Criteria: generateCrudOperations('Catalog.Criteria'),
     Asset: generateCrudOperations('Catalog.Asset'),
     ServiceAsset: generateCrudOperations('Catalog.ServiceAsset'),
@@ -74,52 +88,98 @@ const SMPEvents = {
     ServiceAttribute: generateCrudOperations('Catalog.ServiceAttribute'),
     Topic: generateCrudOperations('Catalog.Topic'),
     Tag: generateCrudOperations('Catalog.Tag'),
-
   },
 
   Accounting: {
-    Invoice: generateCrudOperations('Accounting.Invoice', { paid: 'Accounting.Invoice.paid' }, { paymentFailed: 'Accounting.Invoice.paymentFailed' }),
+    stars: 'accounting.*',
+    Invoice: generateCrudOperations('Accounting.Invoice', { 
+      paid: 'rk.accounting.invoice.paid' 
+    }, { 
+      paymentFailed: 'rk.accounting.invoice.paymentfailed' 
+    }),
     Estimate: generateCrudOperations('Accounting.Estimate'),
     Transaction: generateCrudOperations('Accounting.Transaction'),
     EstimateAsset: generateCrudOperations('Accounting.EstimateAsset'),
-    InvoicePaymentIntent:{ created: 'Accounting.InvoicePaymentIntent.created' },
+    InvoicePaymentIntent: { created: 'rk.accounting.invoicepaymentintent.created' },
   },
 
   Notification: {
-    Notification: generateCrudOperations('Notification.Notification', { read: 'Notification.Notification.read' }),
+    stars: 'notification.*',
+    Notification: generateCrudOperations('Notification.Notification', { 
+      read: 'rk.notification.notification.read' 
+    }),
     NotificationTemplate: generateCrudOperations('Notification.NotificationTemplate'),
-    NotiUser: { created: 'Notification.NotiUser.created' },
-    NotiCommit: { created: 'Notification.NotiCommit.created' },
+    NotiUser: { created: 'rk.notification.notiuser.created' },
+    NotiCommit: { created: 'rk.notification.noticommit.created' },
     NewsletterContact: generateCrudOperations('Notification.NewsletterContact'),
     ContactGroup: generateCrudOperations('Notification.ContactGroup'),
     ContactGroupMembership: generateCrudOperations('Notification.ContactGroupMembership'),
-    Campaign: generateCrudOperations('Notification.Campaign', { readyToCommit: 'Notification.Campaign.readyToCommit' }),
-    Newsletter: generateCrudOperations('Notification.Newsletter', { readyToCommit: 'Notification.Newsletter.readyToCommit' }),
+    Campaign: generateCrudOperations('Notification.Campaign', { 
+      readyToCommit: 'rk.notification.campaign.readytocommit' 
+    }),
+    Newsletter: generateCrudOperations('Notification.Newsletter', { 
+      readyToCommit: 'rk.notification.newsletter.readytocommit' 
+    }),
   },
 
   ReviewComment: {
+    stars: 'reviewcomment.*',
     Criteria: generateCrudOperations('ReviewComment.Criteria'),
     Comment: generateCrudOperations('ReviewComment.Comment'),
     Review: generateCrudOperations('ReviewComment.Review'),
   },
 
   Authentication: {
-    Application: generateCrudOperations('Authentication.Application', {logged: 'Authentication.Application.logged', register: 'Authentication.Application.register', desactivated: 'Authentication.Application.desactivated', logout: 'Authentication.Application.logout'}),
-    ApplicationToken: generateCrudOperations('Authentication.ApplicationToken', {refreshed: 'Authentication.ApplicationToken.refreshed'}),
-    UserToken: generateCrudOperations('Authentication.UserToken', {refreshed: 'Authentication.UserToken.refreshed'}),
-    User: generateCrudOperations('Authentication.User', { deactivated: 'Authentication.User.deactivated', logged: 'Authentication.User.logged', signup: 'Authentication.User.signup', logout: 'Authentication.User.logout' }),
-    AuthZ: { allowed: 'AuthZ.allowed', denied: 'AuthZ.denied', removed: 'AuthZ.removed', added: 'AuthZ.added' },
+    stars: 'authentication.*',
+    Application: generateCrudOperations('Authentication.Application', { 
+      logged: 'rk.authentication.application.logged', 
+      register: 'rk.authentication.application.register', 
+      desactivated: 'rk.authentication.application.desactivated', 
+      logout: 'rk.authentication.application.logout' 
+    }),
+    ApplicationToken: generateCrudOperations('Authentication.ApplicationToken', { 
+      refreshed: 'rk.authentication.applicationtoken.refreshed' 
+    }),
+    UserToken: generateCrudOperations('Authentication.UserToken', { 
+      refreshed: 'rk.authentication.usertoken.refreshed' 
+    }),
+    User: generateCrudOperations('Authentication.User', { 
+      deactivated: 'rk.authentication.user.deactivated', 
+      logged: 'rk.authentication.user.logged', 
+      signup: 'rk.authentication.user.signup', 
+      logout: 'rk.authentication.user.logout' 
+    }),
+    AuthZ: { 
+      allowed: 'rk.authz.allowed', 
+      denied: 'rk.authz.denied', 
+      removed: 'rk.authz.removed', 
+      added: 'rk.authz.added' 
+    },
     ResetPasswordToken: generateCrudOperations('Authentication.ResetPasswordToken'),
     Password: generateCrudOperations('Authentication.Password'),
-    UserInvited: {created: 'Authentication.UserInvited.created'},
+    UserInvited: { 
+      stars: "rk.authentication.userinvited.*", 
+      created: 'rk.authentication.userinvited.created', 
+      canceled: 'rk.authentication.userinvited.canceled', 
+      expired: 'rk.authentication.userinvited.expired'
+    },
   },
 
   Authorization: {
-    Role: generateCrudOperations('Authorization.Role', { deactivated: 'Authorization.Role.revoked' }),
-    Member: { logged: 'Authorization.Member.allowed', refreshed: 'Authorization.Member.denied', logout: 'Authorization.Member.removed', logout: 'Authorization.Member.added' },
+    stars: 'authorization.*',
+    Role: generateCrudOperations('Authorization.Role', { 
+      deactivated: 'rk.authorization.role.revoked' 
+    }),
+    Member: { 
+      logged: 'rk.authorization.member.allowed', 
+      refreshed: 'rk.authorization.member.denied', 
+      logout: 'rk.authorization.member.removed', 
+      added: 'rk.authorization.member.added' 
+    },
   },
 
   Document: {
+    stars: 'document.*',
     Media: generateCrudOperations('Document.Media'),
     MediaFolder: generateCrudOperations('Document.MediaFolder'),
     EstimateMedia: generateCrudOperations('Document.EstimateMedia'),
@@ -129,12 +189,12 @@ const SMPEvents = {
     UserMedia: generateCrudOperations('Document.UserMedia'),
     OrganizationMedia: generateCrudOperations('Document.OrganizationMedia'),
     ServiceMedia: generateCrudOperations('Document.ServiceMedia'),
-    FaqAnswerMedia: generateCrudOperations('Document.FaqAnswerMedia'),
-    FaqQuestionMedia: generateCrudOperations('Document.FaqQuestionMedia'),
-    
+    FaqAnswer: generateCrudOperations('Document.FaqAnswerMedia'),
+    FaqQuestion: generateCrudOperations('Document.FaqQuestionMedia'),
   },
 
   Location: {
+    stars: 'location.*',
     Place: generateCrudOperations('Location.Place'),
   },
 };
